cb167fd8d1933da7c14218d69f8103c7c24b232a
iamdealer02 <sharmaupasana823@gmail.com>
2024-01-27 15:30:51 +0100

    ErrorFix : Changing the Login Route from GET to POST

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authServices = require('../services/auth.services');
 
 router.post('/register', authServices.register );
-router.get('/login', authServices.login);
+router.post('/login', authServices.login);
 router.get('/logout', authServices.logout);
 
 module.exports = router;
