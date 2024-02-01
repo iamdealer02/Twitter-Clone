b240d7557e97ee334ddfc09cd6a5820fabea6269
aarjookhand <03aarjoo@gmail.com>
2024-02-01 15:25:49 +0100

    Merge Conflicts resolves

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import PrivateRoute from './routes/PrivateRoute';
 import PublicRoute from './routes/PublicRoute';
 import HomePage from './views/HomePage';
 import ProfilePage from './views/ProfilePage';
+import MessagePage from './views/MessagePage';
 
 function App() {
   return (
@@ -25,6 +26,9 @@ function App() {
 
 
           
+          <Route element={<PrivateRoute/>}>
+            <Route path="/messages" element={<MessagePage/>}/>
+          </Route>
         </Routes>
   );
 }
